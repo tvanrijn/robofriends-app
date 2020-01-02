cfef08ec977e494adf7ae7fb208a050f1a3b877e
tvanrijn <vanrijn.tineke@gmail.com>
2020-01-02 10:08:25 +0100

    Updates

---
+++ precache-manifest.d6524a3cedc810f90622fffe5e3c429c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fb67be7e25f3f27800518a24cb75429",
+    "url": "/robofriends-app/index.html"
+  },
+  {
+    "revision": "40ba58782b2d095e0146",
+    "url": "/robofriends-app/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "0166907aecf85a93dc1e",
+    "url": "/robofriends-app/static/css/main.45fd6a85.chunk.css"
+  },
+  {
+    "revision": "40ba58782b2d095e0146",
+    "url": "/robofriends-app/static/js/2.a51c3c0d.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/robofriends-app/static/js/2.a51c3c0d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0166907aecf85a93dc1e",
+    "url": "/robofriends-app/static/js/main.747fae5f.chunk.js"
+  },
+  {
+    "revision": "37093de5a047058f350c",
+    "url": "/robofriends-app/static/js/runtime-main.bbdfbbe9.js"
+  },
+  {
+    "revision": "764717d66ec16b37b687ddf28f842ee7",
+    "url": "/robofriends-app/static/media/Cooljazz.764717d6.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/robofriends-app/precache-manifest.0f910bb20d60cf5fc4583db92c1e6247.js"
+  "/robofriends-app/precache-manifest.d6524a3cedc810f90622fffe5e3c429c.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/robofriends-app/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/robofriends-app/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
